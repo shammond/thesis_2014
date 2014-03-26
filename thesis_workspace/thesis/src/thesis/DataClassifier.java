a03d2bb0b64a0d64c50b9a1aab409be414f35675
Sarah Hammond <shammond@hamilton.edu>
2014-03-26 18:33:26 -0400

    Updated DataClassifier

--- thesis_workspace/thesis/src/thesis/DataClassifier.java
+++ thesis_workspace/thesis/src/thesis/DataClassifier.java
@@ -35,7 +35,7 @@ public class DataClassifier {
 	public DataClassifier() {
 	}	
 		
-	static void createArff(File eegFile, File timeData, String arffName, int start, int end) {
+	static void createArff(File eegFile, File timeData, String arffName, int start) {
 		
 		BufferedReader eegReader = null;
 		BufferedReader timeReader = null;
@@ -44,7 +44,6 @@ public class DataClassifier {
 			eegReader = new BufferedReader(new FileReader(eegFile));
 			timeReader = new BufferedReader(new FileReader(timeData));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.exit(0);
 		}
@@ -54,23 +53,22 @@ public class DataClassifier {
 		try {
 			arffWriter = new BufferedWriter(new FileWriter(arffFile));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.exit(0);
 		}
 		
 		// start with relation and define attributes
-		// state: (0,pause),(1,simple,standard),(2,simple,small),(3,complex,standard),(4,complex,small)
+		// state: (1,simple,standard),(2,simple,small),(3,complex,standard),(4,complex,small)
 		firstcolumn = 3;
-		numAttrs = 6;
+		numAttrs = 8;
 		
 		try {
 			arffWriter.write("@relation readingType");
 			arffWriter.newLine();
 			arffWriter.newLine();
 			writeAttributes();
-			arffWriter.write("@attribute speed numeric");
-			arffWriter.newLine();
+			//arffWriter.write("@attribute speed numeric");
+			//arffWriter.newLine();
 			arffWriter.write("@attribute state {1,2,3,4}");  // 4 conditions
 			arffWriter.newLine();
 			arffWriter.newLine();
@@ -79,33 +77,22 @@ public class DataClassifier {
 			e.printStackTrace();
 		}
 		
-		
 		CSVReader reader = new CSVReader(eegReader);
 		List<String[]> eegRows = null;
 		try {
 			eegRows = reader.readAll();
+			reader.close();
+			eegReader.close();
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			System.exit(0);
 		}
 		
 		// get rid of any -99999 values
 		eegRows = nonegnines(eegRows);
-		
-	/*	for (String[] line : eegRows) {
-			for (String item : line)
-				System.out.print(item + ", ");
-			System.out.println();
-		}
-		
-		if (true)
-			return;
-		*/
 
 		// map times to average reading speeds
 		HashMap<Integer,Double> speedMap = createSpeedMap(timeReader);
-		
 		
 		// add data to arff file
 		
@@ -119,12 +106,11 @@ public class DataClassifier {
 		
 			for (String [] line : eegRows) {
 				
-				if (start <= offset && offset <= end ) {
+				if (start <= offset && offset <= start + 1600 ) {
 					// set i to be the column of the first attribute, go until have all attributes
-					//System.out.println("time: " + time);
 					
 					// add the condition as the last attribute
-					if (inRange(time,0,75) || inRange(time,600,675) || inRange(time,900,975) || inRange(time,1400,1475)) {
+					if (/*inRange(time,0,75) ||*/ inRange(time,600,675) || inRange(time,900,975) || inRange(time,1400,1475)) {
 						writeArffLine(line,speedMap.get(time),"1");
 					}
 					else if (inRange(time,100,175) || inRange(time,700,775) || inRange(time,1100,1175) || inRange(time,1200,1275)) {
@@ -146,19 +132,14 @@ public class DataClassifier {
 		}
 		
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			System.exit(0);
 		}
 		
 		
 		try {
-			reader.close();
-			eegReader.close();
-			//timeReader.close();
 			arffWriter.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.exit(0);
 		}
@@ -177,7 +158,8 @@ public class DataClassifier {
 			for (int i=firstcolumn;i<firstcolumn+numAttrs;i++) {
 				arffWriter.write(line[i] + ",");
 			}
-			arffWriter.write(Double.toString(speed) + ",");
+			// with speed
+			//arffWriter.write(Double.toString(speed) + ",");
 			arffWriter.write(condition);
 			arffWriter.newLine();
 		} catch (IOException e) {
@@ -190,12 +172,11 @@ public class DataClassifier {
 	static void writeAttributes() {
 
 			try {
-				for (int i = 0; i < 6; i++) {
+				for (int i = 0; i < numAttrs; i++) {
 					arffWriter.write("@attribute data" + i + " numeric");
 					arffWriter.newLine();
 				}
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 				System.exit(0);
 			}
@@ -208,12 +189,13 @@ public class DataClassifier {
 		
 		for (int line = 2; line < eegdata.size(); line++) {
 			for (int col = 3; col < eegdata.get(line).length; col++) {
-				if (eegdata.get(line)[col].equals("-99999")) {
+				if (Double.parseDouble(eegdata.get(line)[col]) < 0) {
+					
 					double before = 0;
 					if (line > 2)
 						before = Double.parseDouble(eegdata.get(line-1)[col]);
 					int numlines = 0;
-					while ((line+numlines < eegdata.size()) && eegdata.get(line+numlines)[col].equals("-99999")) {
+					while ((line+numlines < eegdata.size()) && Double.parseDouble(eegdata.get(line+numlines)[col]) < 0) {
 						numlines++;
 					}
 					
@@ -309,6 +291,7 @@ public class DataClassifier {
 		//createArff(".//data//183102000_Ref_Class.csv",".//data//justinsbrain.arff",60,1660);
 		JFrame parent = new JFrame();
 		JFileChooser selectFile = new JFileChooser();
+		selectFile.setCurrentDirectory(new File("C:\\Users\\shammond\\Desktop\\Classification_Files"));
 		int fcval = selectFile.showDialog(parent, "Select a classification file.");
 		if (fcval==JFileChooser.APPROVE_OPTION)
 			classification = selectFile.getSelectedFile();
@@ -321,7 +304,7 @@ public class DataClassifier {
 		else
 			System.exit(0);
 		
-		String subjectName = (String)JOptionPane.showInputDialog(parent,"Enter Decision Tree Class Name:", "File Name",JOptionPane.PLAIN_MESSAGE);
+		String subjectName = (String)JOptionPane.showInputDialog(parent,"Enter Subject Name:", "File Name",JOptionPane.PLAIN_MESSAGE);
 		if (subjectName==null)
 			System.exit(0);
 		int start = 0;
@@ -334,17 +317,15 @@ public class DataClassifier {
 		}
 		
 		//createArff(".//src//thesis//184103000_Classification.csv",".//logs//3-8-2014-adamd-clicks","adamd-test.arff",57,1657);
-		// change
 		
-		createArff(classification,clicks,subjectName,start,start+1600);
+		createArff(classification,clicks,subjectName,start);
 		
-		if (true)
-			return;
+	//	if (true)
+		//	return;
 		
 		try {
 			breader = new BufferedReader(new FileReader(".//arffFiles//" + subjectName +".arff"));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.exit(0);
 		}
@@ -361,7 +342,6 @@ public class DataClassifier {
 			data = new Instances(breader);
 			breader.close();
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			System.exit(0);
 		}
@@ -371,7 +351,6 @@ public class DataClassifier {
 		try {
 			dtree.buildClassifier(data);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.exit(0);
 		}
@@ -383,7 +362,6 @@ public class DataClassifier {
 			bsc.newLine();
 			bsc.newLine();
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			System.exit(0);
 		}
@@ -393,10 +371,18 @@ public class DataClassifier {
 			bsc.write(dtree.toSource(subjectName + "Classifier"));
 			bsc.close();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.exit(0);
 		}
 		
+		/*try {
+			System.out.println(dtree.graph());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		*/
+		return;
 	}
 
 }
